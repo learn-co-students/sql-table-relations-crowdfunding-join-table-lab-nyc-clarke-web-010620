db326510f116aa983f323aa9353e93bbc8eacd77
arichards4814 <arichards4814@gmail.com>
2020-01-15 16:18:17 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,58 @@
+INSERT INTO users (name, age)
+VALUES ("Boris", 20);
+
+INSERT INTO users (name, age)
+VALUES
+("Boris", 20), ("Boris", 20), ("Boris", 20),
+("Boris", 20), ("Boris", 20), ("Boris", 20),
+("Boris", 20), ("Boris", 20), ("Boris", 20),
+("Boris", 20), ("Boris", 20), ("Boris", 20),
+("Boris", 20), ("Boris", 20), ("Boris", 20), 
+("Boris", 20), ("Boris", 20), ("Boris", 20),
+("Boris", 20);
+
+INSERT INTO projects (title, category, start_date, funding_goal, start_date, end_date)
+VALUES
+(1,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(2,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(3,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(4,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(5,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(6,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(7,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(8,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(9,"Proj", "cat", 50000, "01/02/2019", "Test"),
+(10,"Proj", "cat", 50000, "01/02/2019", "Test");
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4),
+(500, 2, 4);
